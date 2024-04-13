18d8ce5d61d2350def41543b763ea15bbb6b9b55
AloizioMacedo <atsam2@hotmail.com>
2024-04-13 12:54:31 -0300

    Allow ignoring hidden files

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,16 @@ use anyhow::{anyhow, Result};
 use clap::Parser;
 use pydcstrngs::parse_file_contents;
 use rayon::prelude::*;
+use walkdir::DirEntry;
 
 #[derive(Parser)]
 #[command(version, about, long_about=None)]
 struct Args {
     path: String,
+
+    #[arg(long, default_value_t = true, alias = "nh")]
+    /// Will ignore hidden files.
+    ignore_hidden: bool,
 
     #[arg(long, default_value_t = false, alias = "nd")]
     /// Will consider an error for a docstring to be absent.
@@ -18,9 +23,15 @@ struct Args {
     /// Will consider an error for an "Args" section to be absent.
     forbid_no_args_in_docstring: bool,
 
-    #[arg(long, default_value_t = false, alias = "ud")]
+    #[arg(long, default_value_t = false, alias = "nu")]
     /// Will consider an error for an arg in docstring to be untyped.
     forbid_untyped_docstrings: bool,
+}
+
+fn is_hidden(e: &DirEntry) -> bool {
+    e.file_name()
+        .to_str()
+        .map_or(false, |s| s.starts_with('.') && s != ".")
 }
 
 fn main() -> Result<()> {
@@ -35,35 +46,45 @@ fn main() -> Result<()> {
 
         let global_success = AtomicBool::new(true);
 
-        walk.into_iter().par_bridge().for_each(|entry| {
-            let Ok(entry) = entry else {
-                return;
-            };
-
-            if entry.path().is_file()
-                && entry.path().extension() == Some(std::ffi::OsStr::from_bytes("py".as_bytes()))
-            {
-                let span = tracing::error_span!(
-                    "file",
-                    file_name = entry.path().as_os_str().to_str().unwrap()
-                );
-
-                _ = span.enter();
-
-                let Ok(success) = parse_file(
-                    entry.path(),
-                    args.forbid_no_docstring,
-                    args.forbid_no_args_in_docstring,
-                    args.forbid_untyped_docstrings,
-                ) else {
+        walk.into_iter()
+            .filter_entry(|e| {
+                if args.ignore_hidden {
+                    !is_hidden(e)
+                } else {
+                    true
+                }
+            })
+            .par_bridge()
+            .for_each(|entry| {
+                let Ok(entry) = entry else {
                     return;
                 };
 
-                if !success {
-                    global_success.swap(false, std::sync::atomic::Ordering::Relaxed);
+                if entry.path().is_file()
+                    && entry.path().extension()
+                        == Some(std::ffi::OsStr::from_bytes("py".as_bytes()))
+                {
+                    let span = tracing::error_span!(
+                        "file",
+                        file_name = entry.path().as_os_str().to_str().unwrap()
+                    );
+
+                    _ = span.enter();
+
+                    let Ok(success) = parse_file(
+                        entry.path(),
+                        args.forbid_no_docstring,
+                        args.forbid_no_args_in_docstring,
+                        args.forbid_untyped_docstrings,
+                    ) else {
+                        return;
+                    };
+
+                    if !success {
+                        global_success.swap(false, std::sync::atomic::Ordering::Relaxed);
+                    }
                 }
-            }
-        });
+            });
 
         global_success.into_inner()
     } else {
